16c5f160482aba6857236a00d4fd1af9dec2e862
Mark Nyanjui <mark.warunge@student.moringaschool.com>
2024-04-09 21:14:19 +0300

    Complete Lab

--- index.js
+++ index.js
@@ -9,6 +9,62 @@
  for you to use if you need it!
  */
 
+function createEmployeeRecord(firstName, familyName, title, payPerHour) {
+    return {
+
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arrays) {
+    return arrays.map(arr => createEmployeeRecord(...arr))
+}
+
+function createTimeInEvent(employeeRecord, dateTime) {
+    if (!dateTime ) return;
+    const [date, hour] = dateTime.split(' ')
+    employeeRecord.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date
+    })
+    return employeeRecord
+}
+
+function createTimeOutEvent(employeeRecord, dateTime) {
+    if (!dateTime ) return;
+    const [date, hour] = dateTime.split(' ')
+    if (!dateTime) return
+    employeeRecord.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date
+    })
+    return employeeRecord
+}
+
+function hoursWorkedOnDate(employeeRecord, date) {
+    const timeInEvents = employeeRecord.timeInEvents.find(e => e.date === date);
+    const timeOutEvents = employeeRecord.timeOutEvents.find(e => e.date === date);
+    if (!timeInEvents || !timeOutEvents ) return 0;
+    const hoursWorked = (timeOutEvents[0].hour - timeInEvents[0].hour) / 100;
+    return hoursWorked;
+}
+
+
+function wagesEarnedOnDate(employeeRecord, date) {
+    const hoursWorked = hoursWorkedOnDate(employeeRecord, date)
+    const wagesEarned = hoursWorked * employeeRecord.payPerHour
+    return wagesEarned
+}
+
+
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -20,4 +76,15 @@ const allWagesFor = function () {
 
     return payable
 }
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(e => e.firstName === firstName)
+}
+
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((acc, employee) => acc + allWagesFor.call(employee), 12480)
+
+}
+
+
 
